156202581450079de1e27e822fa72b35fc662ee7
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-04-01 12:07:26 -0300

    add methods

---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./src');
+
+app.listen(3333, () => {
+  console.log('App listening on port 3333');
+});
---
+++ src/petshop.js
@@ -0,0 +1,136 @@
+const moment = require('moment');
+const fs = require('fs');
+
+let bancoDados = fs.readFileSync('../database.json');
+
+bancoDados = JSON.parse(bancoDados);
+
+const petshop = {
+  atualizarBanco: () => {
+    let petsAtualizado = JSON.stringify(bancoDados);
+    fs.writeFileSync('database.json', petsAtualizado, 'utf-8');
+  },
+  listarPets: () => {
+    bancoDados.pets.forEach((pet) => {
+      let { nome, idade, tipo, raca, vacinado } = pet;
+      console.log(
+        `${nome}, ${idade}, ${tipo}, ${raca}, ${
+          vacinado ? 'vacinado' : 'não vacinado'
+        }`
+      );
+    });
+
+    for (const servico of pet.servicos) {
+      console.log(`${servico.data} - ${servico.nome}`);
+    }
+  },
+  vacinarPet: (pet) => {
+    if (!pet.vacinado) {
+      pet.vacinado = true;
+      console.log(`${pet.nome} foi vacinado com sucesso`);
+    } else {
+      console.log(`Ops, ${pet.nome} já estava vacinado.`);
+    }
+    atualizarBanco();
+  },
+  campanhaVacina: () => {
+    var petsVacinados = 0;
+
+    bancoDados.pets.map((pet) => {
+      if (!pet.vacinado) {
+        vacinarPet(pet);
+        petsVacinados++;
+      }
+      return pet;
+    });
+
+    console.log(
+      `Foram vacinados ${petsVacinados} pets na campanha de vacinação`
+    );
+  },
+  adicionarPet: (...novosPets) => {
+    novosPets.forEach((novoPet) => {
+      bancoDados.pets.push(novoPet);
+    });
+
+    atualizarBanco();
+    novosPets.forEach((pet) => {
+      console.log(`${pet.nome} foi adicionado com sucesso!`);
+    });
+  },
+  darBanhoPet: (pet) => {
+    pet.servicos.push({
+      nome: 'dar banho',
+      data: moment().format('DD-MM-YYYY'),
+    });
+    atualizarBanco();
+    console.log(`${pet.nome} está de banho tomado`);
+  },
+  tosarPet: (pet) => {
+    pet.servicos.push({
+      nome: 'tosa',
+      data: moment().format('DD-MM-YYYY'),
+    });
+    atualizarBanco();
+    console.log(`${pet.nome} está com o cabelinho na régua`);
+  },
+  apararUnhasPet: (pet) => {
+    pet.servicos.push({
+      nome: 'aparar unhas',
+      data: moment().format('DD-MM-YYYY'),
+    });
+    atualizarBanco();
+    console.log(`${pet.nome} está de unhas aparadas`);
+  },
+  atenderCliente: (pet, servico) => {
+    console.log(`Olá, ${pet.tutor}, os serviços realizados foram:`);
+    servico ? servico(pet) : console.log('só vim dar uma olhadinha');
+    console.log('Volte sempre!');
+  },
+  buscarPet: (nomePet) => {
+    let foundPet = bancoDados.pets.find((pet) => {
+      return pet.nome == nomePet;
+    });
+    return foundPet
+      ? foundPet
+      : `Desculpe, ${nomePet} não foi encontrado no sistema`;
+  },
+  filtrarPet: (petEspecie) => {
+    let petsEncontrados = bancoDados.pets.filter((pet) => {
+      return pet.especie == petEspecie;
+    });
+
+    return petsEncontrados;
+  },
+  clientePremium: (pet) => {
+    let { nome } = pet;
+    let nServicos = pet.servicos.length;
+
+    if (nServicos > 5) {
+      console.log(
+        `Olá, ${nome}! Você é um cliente especial e ganhou um descontão!`
+      );
+    } else {
+      console.log(`Olá, ${nome}! Você ainda não tem descontos disponiveis!`);
+    }
+  },
+  contatoTutor: (pet) => {
+    let { nome, tutor, contato } = pet;
+
+    return `Tutor: ${tutor}
+            Contato: ${contato}
+            Pet: ${nome}`;
+  },
+  filtrarTutor: (nomeTutor) => {
+    let petsTutor = bancoDados.pets.filter((pet) => {
+      return pet.tutor == nomeTutor;
+    });
+
+    console.log(`Pets do tutor ${nomeTutor}:`);
+    petsTutor.forEach((pet) => {
+      console.log(`${pet.nome} - ${pet.tipo}`);
+    });
+  },
+};
+
+module.exports = petshop;
